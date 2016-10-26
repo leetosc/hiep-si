d2569cdf72b04122865d42985e9fbacb97cdda54
Denny <leetosc@hotmail.com>
2016-10-26 00:41:00 -0500

    faster fade in

--- public/js/main2.js
+++ public/js/main2.js
@@ -22,7 +22,7 @@ function closePanel(type) {
 function startup(){
 	// appendLog("Started up");
     window.sr = ScrollReveal();
-    sr.reveal('.animatedpanel',{reset:true, viewFactor:0.1, easing:'ease-in-out'});
+    sr.reveal('.animatedpanel',{reset:true, viewFactor:0.1, easing:'ease-in-out', duration:250});
 	$.get("getData", function(payload){
 		var tr;
         for (var i = 0; i < payload.length; i++) {
